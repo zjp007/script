60317dca47a7f1494fb128deca2260b8bcacefca
zjp <906030149@qq.com>
2021-03-24 18:34:47 +0800

    新增去除视频水印

--- JS/童程脚本.user.js
+++ JS/童程脚本.user.js
@@ -11,34 +11,25 @@
 
 (function() {
     'use strict';
-    //var jq = document.createElement('script');
-    //jq.src = "https://cdn.staticfile.org/jquery/3.4.1/jquery.min.js";
-    //document.getElementsByTagName('head')[0].appendChild(jq);
     console.log("童程脚本")
     // 点击继续学习方法
     function clickStudyBtn(){
-        //if (btn != null){
         var timeFlag = setInterval (function(){
             var dvWarningView = document.getElementById("dvWarningView");
             //var btn2 = $("#dvWarningView");
             if(dvWarningView != null && $(dvWarningView).find("input") != null){
                 $(dvWarningView).find("input").click();
-                console.log("点击继续学习");
-            }else{
-                console.log("未找到继续学习按钮");
             }
             var replaybtn = document.getElementById("replaybtn");
             //var btn2 = $("#dvWarningView");
             if(replaybtn != null && $(replaybtn).find("input") != null){
                 $(replaybtn).remove();
-                console.log("去掉暂停按钮");
-            }else{
-                console.log("未找到暂停按钮");
+            }
+			var shuiYin = $("div.jw-marquee.jw-marquee-extension")
+			if(shuiYin != null && shuiYin != ''){
+                shuiYin.remove();
             }
         },5000);
-        //}else{
-        //    console("未找到继续学习按钮");
-        //}
     }
     // Your code here...
     $(document).ready(function () {
